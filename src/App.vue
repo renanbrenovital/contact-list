874790db3b4369264f24a73ac890b44ee33ea245
Renan Vital <renanbreno@gmail.com>
2020-08-05 20:35:08 -0300

    create UserList component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <UserList />
   </div>
 </template>
 
 <script>
+import UserList from './components/UserList.vue'
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    UserList
+  }
 }
 </script>
 
