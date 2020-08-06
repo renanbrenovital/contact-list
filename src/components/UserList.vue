cefee0dc079b0e3c8a5ec225c6aed4f8b287d431
Renan Vital <renanbreno@gmail.com>
2020-08-06 11:19:01 -0300

    create simple form to add contact

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -22,6 +22,39 @@
         </span>
       </li>
     </ul>
+    <div class="form">
+      <label class="mdc-text-field mdc-text-field--filled">
+        <span class="mdc-text-field__ripple"></span>
+        <input class="mdc-text-field__input" type="text" aria-labelledby="cpf" v-model="form.cpf">
+        <span class="mdc-floating-label" id="cpf">CPF</span>
+        <span class="mdc-line-ripple"></span>
+      </label>
+      <label class="mdc-text-field mdc-text-field--filled">
+        <span class="mdc-text-field__ripple"></span>
+        <input class="mdc-text-field__input" type="text" aria-labelledby="name" v-model="form.name">
+        <span class="mdc-floating-label" id="cpf">Nome Completo</span>
+        <span class="mdc-line-ripple"></span>
+      </label>
+      <label class="mdc-text-field mdc-text-field--filled">
+        <span class="mdc-text-field__ripple"></span>
+        <input class="mdc-text-field__input" type="text" aria-labelledby="email" v-model="form.email">
+        <span class="mdc-floating-label" id="email">E-mail</span>
+        <span class="mdc-line-ripple"></span>
+      </label>
+      <label class="mdc-text-field mdc-text-field--filled">
+        <span class="mdc-text-field__ripple"></span>
+        <input class="mdc-text-field__input" type="text" aria-labelledby="phone" v-model="form.phone">
+        <span class="mdc-floating-label" id="phone">Telefone</span>
+        <span class="mdc-line-ripple"></span>
+      </label>
+      <div class="mdc-touch-target-wrapper">
+        <button class="mdc-button mdc-button--touch" v-on:click="createUser">
+          <div class="mdc-button__ripple"></div>
+          <span class="mdc-button__label">Cadastrar Contato</span>
+          <div class="mdc-button__touch"></div>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,6 +63,12 @@ export default {
   name: 'UserList',
   data() {
     return {
+      form: {
+        cpf: '',
+        name: '',
+        email: '',
+        phone: ''
+      },
       users: []
     }
   },
@@ -49,6 +88,14 @@ export default {
           .catch(error => console.error(error))
       } 
     },
+    createUser() {
+      this.users.push({
+        cpf: this.form.cpf,
+        name: this.form.name,
+        email: this.form.email,
+        phone: this.form.phone
+      });
+    },
     deleteUser(index) {
       this.users.splice(index, 1);
     },
@@ -58,7 +105,7 @@ export default {
   },
   watch: {
     users() {
-      localStorage.setItem('users', JSON.stringify(this.users))
+      localStorage.users = JSON.stringify(this.users)
     }
   },
 }
@@ -67,5 +114,16 @@ export default {
 <style scoped>
 .mdc-list {
   margin-top: 100px;
+}
+.form {
+  height: 50vh;
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+label {
+  margin-bottom: 10px;
 }
 </style>
