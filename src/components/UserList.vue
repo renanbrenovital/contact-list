a2b5f97b62466cb54d26dea3f848a01b400c9da7
Renan Vital <renanbreno@gmail.com>
2020-08-05 23:26:34 -0300

    ajust design and create action buttons

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <h1>Usuários</h1>
-    <ul class="mdc-list">
-      <li class="mdc-list-item" tabindex="0" v-for="user in users" v-bind:key="user.cpf">
-        <span class="mdc-list-item__ripple"></span>
-        <span class="mdc-list-item__text">{{user.name}}</span>
+    <header class="mdc-top-app-bar mdc-top-app-bar--short">
+      <div class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <span class="mdc-top-app-bar__title">Contatos</span>
+        </section>
+      </div>
+    </header>
+    <ul class="mdc-list inline-demo-list mdc-list--avatar-list">
+      <li class="mdc-list-item mdc-ripple-upgraded" v-for="(user, index) in users" v-bind:key="user.cpf">
+        <span class="material-icons mdc-card__action mdc-card__action--icon">
+          person
+        </span>      
+        <span class="mdc-list-item__text">
+          {{user.name}}
+        </span>
+        <span aria-hidden="true" class="mdc-list-item__meta">
+          <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="Editar Usuário" v-on:click="editUser(index)">edit</button>
+          <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon" title="Deletar Usuário" v-on:click="deleteUser(index)">delete</button>
+        </span>
       </li>
     </ul>
   </div>
@@ -36,9 +50,20 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    deleteUser(index) {
+      this.users.splice(index, 1);
+    },
+    editUser(index) {
+      console.log('edit user:', index);
+    }
   }
 }
 </script>
 
 <style scoped>
+.mdc-list {
+  margin-top: 100px;
+}
 </style>
